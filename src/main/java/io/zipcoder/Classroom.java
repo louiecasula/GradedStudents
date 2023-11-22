f5498b4757f2ca188100182c9c6add7e787d9a3e
louie <louiecasula@gmail.com>
2023-11-22 18:34:14 -0500

    Set up part 2, gotta finish last methods and create tests.

--- src/main/java/io/zipcoder/Classroom.java
+++ src/main/java/io/zipcoder/Classroom.java
@@ -1,4 +1,55 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.TreeMap;
+
 public class Classroom {
+    ArrayList<Student> students;
+    int maxNumberOfStudents;
+    public Classroom(int maxNumberOfStudents){
+        this.maxNumberOfStudents = maxNumberOfStudents;
+    }
+
+    public Classroom(ArrayList<Student> students){
+        this.students = students;
+    }
+
+    public Classroom(){
+        this.maxNumberOfStudents = 30;
+        this.students = new ArrayList<>();
+    }
+
+    public ArrayList<Student> getStudents(){
+        return students;
+    }
+
+    public Double getAverageExamScore(){
+        Iterator<Student> it = students.iterator();
+        double sum = 0.0;
+        while (it.hasNext()){
+            sum += it.next().getAverageExamScore();
+        }
+        return sum / students.size();
+    }
+
+    public void addStudent(Student student){
+        this.students.add(student);
+    }
+
+    public void removeStudent(String firstName, String lastName){
+        for (Student s: students){
+            if (s.getFirstName().equals(firstName) && s.getLastName().equals(lastName)){
+                this.students.remove(s);
+            }
+        }
+    }
+
+    public ArrayList<Student> getStudentsByScore(){
+        return new ArrayList<Student>();
+    }
+
+    public TreeMap<Student, String> getGradeBook(){
+        return new TreeMap<>();
+    }
 }
--- src/test/java/io/zipcoder/ClassroomTest.java
+++ src/test/java/io/zipcoder/ClassroomTest.java
@@ -1,4 +1,14 @@
 package io.zipcoder;
 
+import org.junit.Test;
+
+import java.util.Random;
+
 public class ClassroomTest {
+    @Test
+    public void poo(){
+        Random random = new Random();
+        int choice = random.nextInt(3) + 1;
+        System.out.println(choice);
+    }
 }
